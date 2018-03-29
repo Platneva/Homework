6f0a14a36ad71beac9454a68a400d186aaee5a4d
Platneva <36657891+Platneva@users.noreply.github.com>
2018-03-29 14:14:21 +0300

    Add files via upload

---
+++ HW3.js
@@ -0,0 +1,27 @@
+function stringMaxSub(s) {
+
+	for (let k = s.length; k > 1; k--) {
+		if (s.length % k != 0) 
+			continue;
+
+	
+		let t_len = s.length / k;
+		let ok = true;
+		for (let i = 0; i < s.length - t_len; i += t_len) {
+			let s1 = s.substring(i, i + t_len);
+			let s2 = s.substring(i + t_len, i + 2 * t_len);
+			ok = ok && (s1 === s2)
+		}
+	
+		if (ok)
+			return k;
+	}
+
+	
+	return 1;
+}
+
+console.log('test 1');
+console.log("abcabcabcabc = ", stringMaxSub("abcabcabcabc"));
+console.log("aaaaaaaaaaaa = ", stringMaxSub("aaaaaaaaaaaa"));
+console.log("abcdefgh = ", stringMaxSub("abcdefgh"));
