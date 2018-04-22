446fe7c1cce52cf1ddf0650fc49b5ae8099b2fd6
Platneva <36657891+Platneva@users.noreply.github.com>
2018-04-22 10:53:28 +0300

    Update WeightGraph.js

--- WeightGraph.js
+++ WeightGraph.js
@@ -1,5 +1,5 @@
 function WeightGraph(corner_list) {
-	
+	// Строим граф из списка ребер
 	let graph = {};
 	corner_list.forEach((corner) => {
 		let p1 = corner[0];
@@ -11,23 +11,24 @@ function WeightGraph(corner_list) {
 		if (!graph[p2]) {
 			graph[p2] = {};
 		}
-
+		// В условии не ясно - направленный у нас граф или не направленный
+		// Если граф направленный, то нужна только первая строка, если не направленный - то обе
 		graph[p1][p2] = weight;
 		graph[p2][p1] = weight;
 	});
 
-	
+	// выводим для граф для отладки
 	console.log("\nweight graph = \n", graph);
 
-	
+	// Алгоритм поиска кратчайшего пути (алгоритм Дейскстры) 
 	this.findPath = function (startNode, endNode) {
-		let nodes = {}; 
+		let nodes = {}; // Список вершин с весами и предыдущей вершиной (нужна для построения пути)
 
 		let node = startNode;
 		nodes[node] = { weight : 0, prev : null, finish : false }
 
-		while (node != null) { 
-			
+		while (node != null) { // Пока есть следующая вершина
+			// Смотрим все соседей
 			for (next in graph[node]) {
 				let new_weight = nodes[node].weight + graph[node][next];
 				if (!nodes[next] || nodes[next].weight > new_weight) {
@@ -35,10 +36,10 @@ function WeightGraph(corner_list) {
 				}
 			}
 
-			
+			// Помечаем вершину как пройденную
 			nodes[node].finish = true;
 			
-			
+			// Ищем следующую не пройденную вершину c минимальным весом
 			node = null;
 			let min = Infinity;
 			for (next in nodes) {
@@ -49,11 +50,11 @@ function WeightGraph(corner_list) {
 			}
 		}
 
-		if (!nodes[endNode]) { 
+		if (!nodes[endNode]) { // Если в списке пройденных вершин нет endNode - значит она недостижима
 			return "path from " + startNode + " to " + endNode + " not found";
 		} 
 
-		
+		// Строим путь путем обратного прохода
 		let ss = "" + endNode + "  =  " + nodes[endNode].weight;
 		node = nodes[endNode].prev
 		while (node != null) {
@@ -65,16 +66,16 @@ function WeightGraph(corner_list) {
 
 }
 
-
+// Граф из задания
 let graph2 = new WeightGraph( [[0, 3, 5], [1, 3, 11], [2, 3, 56], [4, 3, 77], [5, 4, 89]] );
 console.log(graph2.findPath(3, 3));
 console.log(graph2.findPath(1, 5));
 
-
+// Граф из статьи в википедии
 let graph3 = new WeightGraph([ [1, 2, 7], [1, 3, 9], [1, 6, 14], [2, 3, 10], [2, 4, 15], [3, 4, 11], [3, 6, 2], [4, 5, 6], [5, 6, 9] ]);
 console.log(graph3.findPath(1, 5));
 console.log(graph3.findPath(1, 4));
 
-
+// Поиск в несвязанном графе, который может закончиться с ошибкой
 let graph4 = new WeightGraph([ [1, 2, 1], [3, 4, 2]]);
 console.log(graph4.findPath(1, 3));
